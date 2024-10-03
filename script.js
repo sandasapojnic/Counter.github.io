c4585ec0d85fcd7ddd918ab0f2e70368bab681d2
“alexandrasapojnic” <sanda.sapojnic@gmail.com>
2024-10-03 21:47:27 +0300

    Refactor the methods

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+const counter = document.getElementById('counter');
+const btnIncrease = document.getElementById('increase');
+const btnDecrease = document.getElementById('decrease');
+const btnReset = document.getElementById('reset');
 let count = 0;
+
 
 function increment() {
     count++;
-    document.getElementById('counter').innerText = count;
+    counter.textContent = count;
 }
 
 function decrement() {
     count--;
-    document.getElementById('counter').innerText = count;
+    counter.textContent = count;
 }
 
 function reset() {
     count = 0;
-    document.getElementById('counter').innerText = count;
+    counter.textContent = count;
 }
+
+btnIncrease.addEventListener('click', increment);
+btnDecrease.addEventListener('click', decrement);
+btnReset.addEventListener('click', reset);
